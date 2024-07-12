fb9c38ec8d9a2206cbd9248fbf8e6e1e231e1a2f
Yasuhiro Yamada <34151714+tdc-yamada-ya@users.noreply.github.com>
2024-07-12 10:01:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("main")
+import sqlite3
+
+def f(name: str):
+  con = sqlite3.connect("tutorial.db")
+  cur = con.cursor()
+  cur.execute("INSERT INTO foo(name) VALUES (" + name + ")")
+
+if __name__ == "__main__":
+  print("main")
